d37d32ef9d30e36fd9e198e03582fedd6e35e301
Youssefshawky969 <168371773+Youssefshawky969@users.noreply.github.com>
2025-07-21 23:51:09 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
 provider "aws" {
   region = var.aws_region
 }
+
+
+
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "Youssef-shawky"
+    workspaces {
+      name = "ecs-task"
+    }
+  }
+}
